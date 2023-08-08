12f97c20d870145be0747ca63c2f15ec7cd77051
smertcanyilmaz <yilmazsmertcan@gmail.com>
2023-08-08 18:01:50 +0300

    add hero section

---
+++ loopstudios_website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   "hero-pattern": "url('./images/desktop/image-hero.jpg')",
+      // },
+    },
+  },
+  plugins: [],
+};
